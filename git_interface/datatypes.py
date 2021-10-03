b5e9ff220fc10ee22434955ddec2689c151e227b
Leo Spratt <enchant97@users.noreply.github.com>
2021-10-03 22:26:58 +0100

    add more git functionality

--- git_interface/datatypes.py
+++ git_interface/datatypes.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
+from enum import Enum
 
 
 @dataclass
@@ -8,3 +9,9 @@ class Log:
     author_email: str
     commit_date: datetime
     subject: str
+
+
+class ArchiveTypes(Enum):
+    TAR = "tar"
+    TAR_GZ = "tar.gz"
+    ZIP = "zip"
--- git_interface/utils.py
+++ git_interface/utils.py
@@ -2,7 +2,8 @@ import subprocess
 from pathlib import Path
 from typing import Optional
 
-from .exceptions import AlreadyExistsException
+from .datatypes import ArchiveTypes
+from .exceptions import AlreadyExistsException, GitException
 
 
 def init_repo(
@@ -22,4 +23,35 @@ def init_repo(
         args.append("--bare")
     if default_branch:
         args.append(f"--initial-branch={default_branch}")
-    subprocess.run(args).check_returncode()
+    process = subprocess.run(args)
+    if process.returncode != 0:
+        raise GitException(process.stderr.decode())
+
+
+def get_description(git_repo: Path) -> str:
+    with open(git_repo / "description", "r") as fo:
+        return fo.read()
+
+
+def set_description(git_repo: Path, description: str):
+    with open(git_repo / "description", "w") as fo:
+        fo.write(description)
+
+
+def run_maintenance(git_repo: Path):
+    args = ["git", "-C", str(git_repo), "maintenance", "run"]
+    process = subprocess.run(args, capture_output=True)
+    if process.returncode != 0:
+        raise GitException(process.stderr.decode())
+
+
+def get_archive(git_repo: Path, archive_type: ArchiveTypes, tree_ish: str = "HEAD") -> bytes:
+    # this allows for strings to be passed
+    if isinstance(archive_type, ArchiveTypes):
+        archive_type = archive_type.value
+    process = subprocess.run(
+        ["git", "-C", str(git_repo), "archive", f"--format={archive_type}", tree_ish],
+        capture_output=True)
+    if process.returncode != 0:
+        raise GitException(process.stderr.decode())
+    return process.stdout
