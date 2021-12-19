f6bc51a6aafd1fa035fbdb198bde7832b3d66812
Leo Spratt <enchant97@users.noreply.github.com>
2021-12-19 16:15:19 +0000

    fix regex

--- git_interface/constants.py
+++ git_interface/constants.py
@@ -1,12 +1,12 @@
 """
 Constants
 """
-EMPTY_REPO_RE = r"fatal: your current branch '\w+' does not have any commits yet"
-UNKNOWN_REV_RE = r"fatal: ambiguous argument '\w+': unknown revision or path not in the working tree."
-BRANCH_ALREADY_EXISTS_RE = r"fatal: A branch named '\w+' already exists."
-BRANCH_NOT_FOUND_RE = r"error: branch '\w+' not found."
-BRANCH_REFNAME_NOT_FOUND_RE = r"error: refname \w+ not found"
-NOT_VALID_OBJECT_NAME_RE = r"fatal: Not a valid object name \w+"
+EMPTY_REPO_RE = r"fatal: your current branch '.+' does not have any commits yet"
+UNKNOWN_REV_RE = r"fatal: ambiguous argument '.+': unknown revision or path not in the working tree."
+BRANCH_ALREADY_EXISTS_RE = r"fatal: A branch named '.+' already exists."
+BRANCH_NOT_FOUND_RE = r"error: branch '.+' not found."
+BRANCH_REFNAME_NOT_FOUND_RE = r"error: refname .+ not found"
+NOT_VALID_OBJECT_NAME_RE = r"fatal: Not a valid object name .+"
 INVALID_OBJECT_NAME = r"fatal: invalid object name '.+'"
 PATH_DOES_NOT_EXIST = r"fatal: path '.+' does not exist in '.+'"
 # git ls-tree <tree-ish>
