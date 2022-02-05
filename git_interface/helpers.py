6c8d06e3595a5c8b55ea2aa39b4cd7731bd46822
Leo Spratt <enchant97@users.noreply.github.com>
2022-02-05 20:45:32 +0000

    add async subprocess helper

--- git_interface/helpers.py
+++ git_interface/helpers.py
@@ -2,8 +2,15 @@
 Methods not related to git commands,
 but to help the program function
 """
+import asyncio
+from collections.abc import Iterable
 from pathlib import Path
+from subprocess import CompletedProcess
 from typing import Union
+
+__all__ = [
+    "ensure_path", "subprocess_run"
+]
 
 
 def ensure_path(path_or_str: Union[Path, str]) -> Path:
@@ -14,3 +21,19 @@ def ensure_path(path_or_str: Union[Path, str]) -> Path:
         :return: The value as a pathlib.Path obj
     """
     return path_or_str if isinstance(path_or_str, Path) else Path(path_or_str)
+
+
+async def subprocess_run(args: Iterable[str]) -> CompletedProcess:
+    """
+    Asynchronous alternative to using subprocess.run
+
+        :param args: The  arguments to run (len must be at least 1)
+        :return: The completed process
+    """
+    process = await asyncio.create_subprocess_exec(
+        args[0], *args[1:],
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return CompletedProcess(args, process.returncode, stdout, stderr)
