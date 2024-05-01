b7c249c6c67103373bcecdb7c60207524ac2914d
Leo Spratt <enchant97@users.noreply.github.com>
2024-05-01 19:22:06 +0100

    migrate to hatch

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 from pathlib import Path
+import shutil
 import pytest
 
 
 @pytest.fixture(scope="session")
-def testdata_path() -> Path:
+def testdata_path():
     path = Path("test-data")
     path.mkdir(parents=True, exist_ok=True)
-    return path
+    yield path
+    shutil.rmtree(path)
